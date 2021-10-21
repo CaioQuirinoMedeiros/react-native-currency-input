dd8706dd96727e81f462eae44489000e29e8186e
Andy Phillipson <ajp@mindspring.com>
2021-10-21 01:58:08 -0400

    Lint

--- src/TextWithCursor.tsx
+++ src/TextWithCursor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 
 import type { TextWithCursorProps } from './props';
 import useBlink from './hooks/useBlink';
